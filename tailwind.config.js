58a8735d12a59eaf853793eb2f9282f300633c06
AnujVerdhan04 <anujverdhan049@gmail.com>
2025-01-05 17:34:45 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-/** @type {import('tailwindcss').Config} */
-module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "./pages/**/*.{js,jsx,ts,tsx}",
@@ -17,4 +9,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
